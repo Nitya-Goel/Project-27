7bbc68431fb7f69af333cc2feafa2959b72af200
Nitya-Goel <75160834+Nitya-Goel@users.noreply.github.com>
2021-03-01 18:06:07 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -2,9 +2,9 @@ class bob {
     constructor(x, y,radius) {
       var options = {
           //'isStatic':true,
-          'restitution':1,
-          'friction':0,
-          'density':0.8
+          restitution:1,
+          friction:0,
+          density:0.8
       }
       
       this.radius = radius
